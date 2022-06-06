2115718ff9858232fc617c7cb5e22873ba4d519c
Hsuan <hsuanchang@star-link.tech>
2022-06-06 21:10:41 +0800

    Fix file structure

--- .sh
+++ .sh
@@ -2,4 +2,4 @@
 node ./utils/dict.js
 
 # !/usr/bin/env python
-penelope -i ./output/data.csv -j csv -f TW -t en -p kobo -o ./output/dicthtml-TW-en
+penelope -i ./output/data.csv -j csv -f TW -t en -p kobo -o ./output/kobo/dicthtml-TW-en
